b6ca23c0d8fe777f6c50e980e1547d6c033cb638
abdelkader37 <qrafiabdelkader@gmail.com>
2024-04-18 02:05:29 +0100

    done

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-'''Module for Square class.'''
+'''Module for Rectangle class.'''
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    '''A subclass representing a square.'''
+    '''A subclass representing a rectangle.'''
 
     def __init__(self, size):
         '''Constructor.'''
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-        '''Method for calculating the area of a square.'''
+        '''Method for area of square.'''
         return self.__size ** 2
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
+"""10-square python module."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    '''A subclass representing a rectangle.'''
+    """the grand child class."""
 
     def __init__(self, size):
-        '''Constructor.'''
+        """construcor of the square class."""
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-        '''Method for area of square.'''
-        return self.__size ** 2
+        """return the area of the square."""
+        return self.__size**2
 
     def __str__(self):
-        '''Returns string representation of this square.'''
-        return "[Square] " + str(self.__size) + "/" + str(self.__size)
+        """return a simple string"""
+        return f"[Square] {self.__size}/{self.__size}"
