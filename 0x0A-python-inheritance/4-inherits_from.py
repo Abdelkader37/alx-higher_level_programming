3af52342e5aa638194698f1d39f698be3b9cc659
abdelkader37 <qrafiabdelkader@gmail.com>
2024-04-18 01:30:43 +0100

    done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
+
+
+def inherits_from(obj, a_class):
+    '''Determines if an object is a true subclass of a class.'''
+
+
+return isinstance(obj, a_class) and not isinstance(obj,     return isinstance(obj, a_class) and )
